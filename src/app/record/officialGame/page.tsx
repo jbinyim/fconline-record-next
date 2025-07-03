81167dabd4f8f666198793ee839d12aae1e1dfb9
SONGDONGGEON <songdonggeon0805@gmail.com>
2025-07-03 13:19:51 +0900

    공식경기 더보기 버튼 제작

--- src/app/record/officialGame/page.tsx
+++ src/app/record/officialGame/page.tsx
@@ -33,7 +33,7 @@ const OfficialGame = () => {
       </section>
       <div>
         {hasNextPage && (
-          <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+          <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage} className="mx-auto mt-[45px] block text-center px-[100px] py-[25px] font-bold text-2xl bg-gradient-to-r from-[#50c878] to-[#4ecdc4] rounded-[20px]" >
             {isFetchingNextPage ? "불러오는 중..." : "더보기"}
           </button>
         )}
