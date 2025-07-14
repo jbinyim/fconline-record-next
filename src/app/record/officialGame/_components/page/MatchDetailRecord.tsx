2528f0f15692afadf7332d8d1211a087389e2801
SONGDONGGEON <songdonggeon0805@gmail.com>
2025-07-14 17:05:24 +0900

    매치게임 닉네임쪽 중앙배치, 매치디테일 요소 분리

--- src/app/record/officialGame/_components/page/MatchDetailRecord.tsx
+++ src/app/record/officialGame/_components/page/MatchDetailRecord.tsx
@@ -18,120 +18,73 @@ const MatchDetailRecord = ({ homeDetail, awayDetail }: MatchDetailRecordProps) =
             <h2 className="w-[100px] text-center">팀 기록</h2>
             <span className="w-[100px] text-center">원정팀</span>
           </div>
+          
           <RecordCard
             homeRecord={homeDetail.shoot}
             nameRecord="슈팅"
             awayRecord={awayDetail.shoot}
           />
 
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.effectShoot}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              유효슈팅
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.effectShoot}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.share}%
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              점유율
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.share}%
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.tryPass}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              패스 횟수
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.tryPass}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.SuccessPass}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              패스 성공 횟수
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.SuccessPass}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.foul}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">파울</span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.foul}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.yellowCard}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              옐로우 카드
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.yellowCard}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.redCard}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              레드 카드
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.redCard}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.offside}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              오프사이드
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.offside}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.cornerKick}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              코너킥
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.cornerKick}
-            </span>
-          </div>
-          <div className="w-[80%] tablet:w-[60%] pc:w-[40%] mx-auto text-black flex justify-between">
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {homeDetail.controller}
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              컨트롤러
-            </span>
-            <span className="text-xs tablet:text-sm pc:text-base w-[100px] text-center">
-              {awayDetail.controller}
-            </span>
-          </div>
+          <RecordCard
+            homeRecord={homeDetail.effectShoot}
+            nameRecord="유효슈팅"
+            awayRecord={awayDetail.effectShoot}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.share}
+            nameRecord="점유율"
+            awayRecord={awayDetail.share}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.tryPass}
+            nameRecord="패스 횟수"
+            awayRecord={awayDetail.tryPass}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.SuccessPass}
+            nameRecord="패스 성공 횟수"
+            awayRecord={awayDetail.SuccessPass}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.foul}
+            nameRecord="파울"
+            awayRecord={awayDetail.foul}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.yellowCard}
+            nameRecord="옐로우 카드"
+            awayRecord={awayDetail.yellowCard}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.redCard}
+            nameRecord="레드 카드"
+            awayRecord={awayDetail.redCard}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.offside}
+            nameRecord="오프사이드"
+            awayRecord={awayDetail.offside}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.cornerKick}
+            nameRecord="코너킥"
+            awayRecord={awayDetail.cornerKick}
+          />
+
+          <RecordCard
+            homeRecord={homeDetail.controller}
+            nameRecord="컨트롤러"
+            awayRecord={awayDetail.controller}
+          />
+
         </div>
       </section>
     </div>
